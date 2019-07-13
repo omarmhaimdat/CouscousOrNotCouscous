562d9b9158ecf46ca6019220a1c03bc8aa2aa22e
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-07-13 10:23:19 +0100

    Initial Commit

---
+++ CouscousOrNotCouscousTests/CouscousOrNotCouscousTests.swift
@@ -0,0 +1,34 @@
+//
+//  CouscousOrNotCouscousTests.swift
+//  CouscousOrNotCouscousTests
+//
+//  Created by M'haimdat omar on 13-07-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import XCTest
+@testable import CouscousOrNotCouscous
+
+class CouscousOrNotCouscousTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
